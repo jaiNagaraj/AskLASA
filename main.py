085720df5029cd6425b3758a11ced618ad97e699
jaiNagaraj <103341642+jaiNagaraj@users.noreply.github.com>
2023-07-19 22:40:59 +0000

    Major overhaul - completed sign-in, dynamic navbar, and better post information

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import logging
 import sqlite3
 from dateutil import tz
 from datetime import datetime as dt
+from zoneinfo import ZoneInfo
+import time
 import hashlib   #import the python hash function library
 import hmac		 #import the hmac library
 import random
@@ -18,6 +20,9 @@ app = fk.Flask(
     static_folder="static"
 )
 CLIENT_ID = "453928047443-0bbpt77htqnhkau7npn8ikb5sucb3r0k.apps.googleusercontent.com"
+G_SUITE_DOMAIN_STUDENTS = "stu.austinisd.org"
+G_SUITE_DOMAIN = "austinisd.org"
+app.secret_key = b'\x02H\t\xf7\x82\xf2\x96\xa7"f\x12\xb4\x14\x07]\x82'
 
 
 
@@ -38,8 +43,8 @@ def check_secure_val(h):
 	h_arr = h.split("|")
 	if (len(h_arr) == 1):
 		return None
-	print(h_arr[0])
-	if (h_arr[0] == "None"): return 1
+	#print(h_arr[0])
+	if (h_arr[0] == "None"): return None
 	s = int(h_arr[0])
 	hash = h_arr[1]
 	# return s if the hash_str(s) equals hash, otherwise return None
@@ -92,8 +97,9 @@ def write_perma_post(permapost):
 def write_new_post(content, forum):
 	with get_connection() as con:
 		cursor = con.cursor()
-		dateInUTC = str(dt.now().strftime("%m/%d/%Y %H:%M:%S"))
-		date = to_cst(dateInUTC)
+		cdt = dt.fromtimestamp(time.time(), tz=ZoneInfo("America/Chicago"))
+		print(cdt)
+		date = cdt.strftime("%m/%d/%Y at %I:%M:%S")
 		postInsert = (date,content,"Guest")
 		cookie = fk.request.cookies.get('user_id')
 		if not (cookie is None): id = check_secure_val(cookie)
@@ -102,15 +108,14 @@ def write_new_post(content, forum):
 		else:
 			users = get_users()
 			cookie = cookie.split("|")
-			hashval = cookie[1]
-			print(hashval)
+			id = cookie[0]
 			for user in users:
-				print(user['hashvals'])
-				if user['hashvals'] == hashval:
+				if user['id'] == id:
 					betterPostInsert = (date,content,user['usernames'])
+					print('WE FOUND IT!!!')
 					break
-			cursor.execute("INSERT INTO " + forum + " (create_date,content,user) VALUES (?,?,?)", postInsert)
-		row = cursor.execute("SELECT * FROM "+ forum +" WHERE create_date = \"" + str(date) + "\"")
+			cursor.execute("INSERT INTO " + forum + " (create_date,content,user) VALUES (?,?,?)", betterPostInsert)
+		row = cursor.execute("SELECT * FROM " + forum + " WHERE create_date = \"" + str(date) + "\"")
 		arr = row.fetchall()[0]
 		return arr['id']
 
@@ -253,51 +258,53 @@ def valid_email(email):
 
 @app.route('/signup', methods=["GET", "POST"], strict_slashes=False)
 def signup():
-	if fk.request.method == 'GET':
-		return write_signup()
-	else:
-		user = fk.request.form["user"]
-		password = fk.request.form["pass"]
-		verify = fk.request.form["verify"]
-		email = fk.request.form["email"]
-		userError = ''
-		passError = ''
-		verifyError = ''
-		emailError = ''
-		hasErrors = False
-
-		# Check for invalid input
-		validUser = valid_user(user, get_usernames())
-		if not validUser == "We good":
-			if validUser == "User exists":
-				userError = "Username already taken"
-			else:
-				userError = 'Your username must contain 3-20 characters from this character set: [a-z,A-Z,0-9,_,-]'
-			hasErrors = True
-		if not valid_pass(password):
-			passError = 'Your password must contain 3-20 characters.'
-			hasErrors = True
-		if not password == verify:
-			verifyError = 'Passwords do not match.'
-			hasErrors = True
-		if not valid_email(email):
-			emailError = 'Please enter a valid email.'
-			hasErrors = True
-
-		
-		if (hasErrors):
-			return write_signup(user, password, verify, email, userError, passError, verifyError, emailError)
-		else:
-			h = make_pw_hash(user,password)
-			with get_connection() as con:
-				cursor = con.cursor()
-				userInsert = (user,h,email)
-				cursor.execute("INSERT INTO users (usernames,hashvals,emails) VALUES (?,?,?)", userInsert)
-			res = fk.make_response(fk.redirect(fk.url_for("welcome"), code=302))
-			id = str(get_id_by_username(user))
-			hashCookie = make_secure_val(id)
-			set_user_cookie(res,hashCookie)
-			return res
+	method = fk.request.method
+	return fk.render_template('signup.html')
+#	if fk.request.method == 'GET':
+#		return write_signup()
+#	else:
+#		user = fk.request.form["user"]
+#		password = fk.request.form["pass"]
+#		verify = fk.request.form["verify"]
+#		email = fk.request.form["email"]
+#		userError = ''
+#		passError = ''
+#		verifyError = ''
+#		emailError = ''
+#		hasErrors = False
+#
+#		# Check for invalid input
+#		validUser = valid_user(user, get_usernames())
+#		if not validUser == "We good":
+#			if validUser == "User exists":
+#				userError = "Username already taken"
+#			else:
+#				userError = 'Your username must contain 3-20 characters from this character set: [a-z,A-Z,0-9,_,-]'
+#			hasErrors = True
+#		if not valid_pass(password):
+#			passError = 'Your password must contain 3-20 characters.'
+#			hasErrors = True
+#		if not password == verify:
+#			verifyError = 'Passwords do not match.'
+#			hasErrors = True
+#		if not valid_email(email):
+#			emailError = 'Please enter a valid email.'
+#			hasErrors = True
+#
+#		
+#		if (hasErrors):
+#			return write_signup(user, password, verify, email, userError, passError, verifyError, emailError)
+#		else:
+#			h = make_pw_hash(user,password)
+#			with get_connection() as con:
+#				cursor = con.cursor()
+#				userInsert = (user,h,email)
+#				cursor.execute("INSERT INTO users (usernames,hashvals,emails) VALUES (?,?,?)", userInsert)
+#			res = fk.make_response(fk.redirect(fk.url_for("profile"), code=302))
+#			id = str(get_id_by_username(user))
+#			hashCookie = make_secure_val(id)
+#			set_user_cookie(res,hashCookie)
+#			return res
 
 @app.route('/login', methods=["GET", "POST"], strict_slashes=False)
 def login():
@@ -306,7 +313,7 @@ def login():
 		if not (cookie is None): id = check_secure_val(cookie)
 		if cookie is None or cookie == "" or (id is None):
 			return write_login()
-		return fk.redirect(fk.url_for("welcome"), code=302)
+		return fk.redirect(fk.url_for("profile"), code=302)
 	else:
 		user = fk.request.form["user"]
 		password = fk.request.form["pass"]
@@ -320,23 +327,37 @@ def login():
 		if (hasErrors):
 			return write_login(user, password, loginError)
 		else:
-			res = fk.make_response(fk.redirect(fk.url_for("welcome"), code=302))
+			res = fk.make_response(fk.redirect(fk.url_for("profile"), code=302))
 			id = str(get_id_by_username(user))
 			hashCookie = make_secure_val(id)
 			set_user_cookie(res,hashCookie)
 			return res
 
-@app.route('/auth', methods=["GET","POST"])
+@app.route('/auth', methods=["POST"])
 def auth():
 	method = fk.request.method
+	with get_connection() as con:
+		cursor = con.cursor()
+		cursor.execute("CREATE TABLE IF NOT EXISTS users (id TEXT, usernames TEXT, hashvals TEXT, emails TEXT, names TEXT, pfp TEXT)")
 	if method == "POST":
 		token = fk.request.get_data()
 		user = fk.request.form.get("user",None)
 		password = fk.request.form.get("pass",None)
 		if user is not None and password is not None:
-			print('yummy')
-			print(user,password)
-			return fk.make_response(fk.redirect(fk.url_for("root"), code=302))
+			loginError = ''
+			hasErrors = False
+			if not check_login(user,password):
+				hasErrors = True
+				loginError = "Invalid login"
+			
+			if (hasErrors):
+				return write_login(user, password, loginError)
+			else:
+				res = fk.make_response(fk.redirect(fk.url_for("profile"), code=302))
+				id = str(get_id_by_username(user))
+				hashCookie = make_secure_val(id)
+				set_user_cookie(res,hashCookie)
+				return res
 		token2 = token
 		token_decoded = token2.decode()
 		data = urllib.parse.parse_qs(token_decoded)
@@ -353,26 +374,123 @@ def auth():
 		# No CSRF attack, yay!
 		idinfo = id_token.verify_oauth2_token(data['credential'][0], requests.Request(), CLIENT_ID)
 		print(idinfo)
+		userID = idinfo['sub']
+		email = idinfo['email']
+		name = idinfo['given_name'] + " " + idinfo['family_name']
+		pfpLink = idinfo['picture']
 		print(idinfo.get('hd',"None"))
 		print(idinfo['sub'])
-		return fk.make_response(fk.redirect(fk.url_for("root"), code=302))
+		users = get_users()
+		userFound = False
+		hasUsername = False
+		for user in users:
+			if user['id'] == idinfo['sub']:
+				userFound = True
+				if user['usernames'] is not None and user['usernames'] != "":
+					hasUsername = True
+				break
+		if not userFound:
+			with get_connection() as con:
+				cursor = con.cursor()
+				userInsert = (userID,email,name,pfpLink)
+				cursor.execute("INSERT INTO users (id,emails,names,pfp) VALUES (?,?,?,?)", userInsert)
+		if not hasUsername:
+			res = fk.make_response(fk.redirect(fk.url_for("setup"), code=302))
+			hashCookie = make_secure_val(userID)
+			set_user_cookie(res,hashCookie)
+			return res
+		res = fk.make_response(fk.redirect(fk.url_for("profile"), code=302))
+		hashCookie = make_secure_val(userID)
+		set_user_cookie(res,hashCookie)
+		return res
 
-@app.route('/welcome', methods=["GET", "POST"])
-def welcome():
+
+@app.route('/setup', methods=["GET", "POST"])
+def setup():
+	method = fk.request.method
+	if method == "GET":
+		cookie = fk.request.cookies.get('user_id')
+		if not (cookie is None): id = check_secure_val(cookie)
+		if cookie is None or cookie == "" or (id is None):
+			fk.abort(400,"You are not logged in, so you cannot access that page.")
+		else:
+			return fk.render_template(
+				"setup.html"
+			)
+	else:
+		user = fk.request.form["user"]
+		password = fk.request.form["pass"]
+		verify = fk.request.form["verify"]
+		userError = ''
+		passError = ''
+		verifyError = ''
+		hasErrors = False
+
+		# Check for invalid input
+		validUser = valid_user(user, get_usernames())
+		if not validUser == "We good":
+			if validUser == "User exists":
+				userError = "Username already taken"
+			else:
+				userError = 'Your username must contain 3-20 characters from this character set: [a-z,A-Z,0-9,_,-]'
+			hasErrors = True
+		if not valid_pass(password):
+			passError = 'Your password must contain 3-20 characters.'
+			hasErrors = True
+		if not password == verify:
+			verifyError = 'Passwords do not match.'
+			hasErrors = True
+
+		
+		if (hasErrors):
+			return fk.render_template(
+				"setup.html",
+				user = user,
+				password = password,
+				verify = verify,
+				userError = userError,
+				passError = passError,
+				verifyError = verifyError
+			)
+		else:
+			cookie = fk.request.cookies.get('user_id')
+			userID = str(check_secure_val(cookie))
+			h = make_pw_hash(user,password)
+			with get_connection() as con:
+				cursor = con.cursor()
+				cursor.execute(f"UPDATE users SET usernames = ? WHERE id = '{userID}'", (user,))
+				cursor.execute(f"UPDATE users SET hashvals = ? WHERE id = '{userID}'", (h,))
+			return fk.make_response(fk.redirect(fk.url_for("profile"), code=302))
+
+@app.route('/profile', methods=["GET", "POST"])
+def profile():
 	cookie = fk.request.cookies.get('user_id')
 	id = check_secure_val(cookie)
 	if cookie is None or cookie == "" or (id is None):
 		return (fk.redirect(fk.url_for("login"), code=302))
 
 	users = get_users()
+	name = ""
 	for user in users:
-		if user['id'] == id:
+		if str(user['id']) == str(id):
+			print('yay!')
+			name = user['names']
 			username = user['usernames']
+			email = user['emails']
+			pfp = user['pfp']
 			break
-	return fk.render_template("welcome.html", username=username)
+	fk.session['logged_in'] = True
+	return fk.render_template(
+		"profile.html",
+		name=name,
+		username=username,
+		email=email,
+		pfp=pfp
+	)
 
 @app.route('/logout', methods=["GET","POST"], strict_slashes=False)	
 def logout():
+	fk.session['logged_in'] = False
 	res = fk.make_response(fk.redirect(fk.url_for("login"), code=302))
 	set_user_cookie(res,"")
 	return res
@@ -430,7 +548,7 @@ def ninth():
 		if content == "":
 			with get_connection() as con:
 				cursor = con.cursor()
-				cursor.execute("CREATE TABLE IF NOT EXISTS " + forum + " (id INTEGER PRIMARY KEY, create_date TEXT, content TEXT, user TEXT)")
+				cursor.execute("CREATE TABLE IF NOT EXISTS " + forum + " (id INTEGER PRIMARY KEY, create_date TEXT, date_formatted TEXT, content TEXT, user TEXT)")
 				s = cursor.execute("SELECT * FROM " + forum + " ORDER BY create_date DESC")
 				return fk.render_template(
 					forum + '.html',
@@ -453,7 +571,7 @@ def ninth():
 	# GET REQUEST
 	with get_connection() as con:
 		cursor = con.cursor()
-		cursor.execute("CREATE TABLE IF NOT EXISTS " + forum + " (id INTEGER PRIMARY KEY, create_date TEXT, content TEXT, user TEXT)")
+		cursor.execute("CREATE TABLE IF NOT EXISTS " + forum + " (id INTEGER PRIMARY KEY, create_date TEXT, date_formatted TEXT, content TEXT, user TEXT)")
 		s = cursor.execute("SELECT * FROM " + forum + " ORDER BY create_date DESC")
 		print([x for x in s])
 		return(write_posts(get_posts(forum), forum))
@@ -467,18 +585,32 @@ def tenth():
 	if method == 'POST':
 		content = fk.request.form["content"]
 		if content == "":
+			with get_connection() as con:
+				cursor = con.cursor()
+				cursor.execute("CREATE TABLE IF NOT EXISTS " + forum + " (id INTEGER PRIMARY KEY, create_date TEXT, date_formatted TEXT, content TEXT, user TEXT)")
+				s = cursor.execute("SELECT * FROM " + forum + " ORDER BY create_date DESC")
+				return fk.render_template(
+					forum + '.html',
+					ph_forum = "/" + forum,
+					ph_content = content,
+					ph_error = "Please write something.",
+					posts = get_posts(forum)
+				)
+		elif profanity.contains_profanity(content):
 			return fk.render_template(
 				forum + '.html',
 				ph_forum = "/" + forum,
 				ph_content = content,
-				ph_error = "Please write something."
+				ph_error = "Foul language detected! Use better words.",
+				posts = get_posts(forum)
 			)
 		else:
 			write_new_post(content,forum)
 			return fk.redirect(fk.url_for(forum))
+	# GET REQUEST
 	with get_connection() as con:
 		cursor = con.cursor()
-		cursor.execute("CREATE TABLE IF NOT EXISTS " + forum + " (id INTEGER PRIMARY KEY, create_date TEXT, content TEXT, user TEXT)")
+		cursor.execute("CREATE TABLE IF NOT EXISTS " + forum + " (id INTEGER PRIMARY KEY, create_date TEXT, date_formatted TEXT, content TEXT, user TEXT)")
 		s = cursor.execute("SELECT * FROM " + forum + " ORDER BY create_date DESC")
 		print([x for x in s])
 		return(write_posts(get_posts(forum), forum))
@@ -492,18 +624,32 @@ def eleventh():
 	if method == 'POST':
 		content = fk.request.form["content"]
 		if content == "":
+			with get_connection() as con:
+				cursor = con.cursor()
+				cursor.execute("CREATE TABLE IF NOT EXISTS " + forum + " (id INTEGER PRIMARY KEY, create_date TEXT, date_formatted TEXT, content TEXT, user TEXT)")
+				s = cursor.execute("SELECT * FROM " + forum + " ORDER BY create_date DESC")
+				return fk.render_template(
+					forum + '.html',
+					ph_forum = "/" + forum,
+					ph_content = content,
+					ph_error = "Please write something.",
+					posts = get_posts(forum)
+				)
+		elif profanity.contains_profanity(content):
 			return fk.render_template(
 				forum + '.html',
-				ph_forum="/" + forum,
+				ph_forum = "/" + forum,
 				ph_content = content,
-				ph_error = "Please write something."
+				ph_error = "Foul language detected! Use better words.",
+				posts = get_posts(forum)
 			)
 		else:
 			write_new_post(content,forum)
 			return fk.redirect(fk.url_for(forum))
+	# GET REQUEST
 	with get_connection() as con:
 		cursor = con.cursor()
-		cursor.execute("CREATE TABLE IF NOT EXISTS " + forum + " (id INTEGER PRIMARY KEY, create_date TEXT, content TEXT, user TEXT)")
+		cursor.execute("CREATE TABLE IF NOT EXISTS " + forum + " (id INTEGER PRIMARY KEY, create_date TEXT, date_formatted TEXT, content TEXT, user TEXT)")
 		s = cursor.execute("SELECT * FROM " + forum + " ORDER BY create_date DESC")
 		print([x for x in s])
 		return(write_posts(get_posts(forum), forum))
@@ -517,18 +663,32 @@ def twelfth():
 	if method == 'POST':
 		content = fk.request.form["content"]
 		if content == "":
+			with get_connection() as con:
+				cursor = con.cursor()
+				cursor.execute("CREATE TABLE IF NOT EXISTS " + forum + " (id INTEGER PRIMARY KEY, create_date TEXT, date_formatted TEXT, content TEXT, user TEXT)")
+				s = cursor.execute("SELECT * FROM " + forum + " ORDER BY create_date DESC")
+				return fk.render_template(
+					forum + '.html',
+					ph_forum = "/" + forum,
+					ph_content = content,
+					ph_error = "Please write something.",
+					posts = get_posts(forum)
+				)
+		elif profanity.contains_profanity(content):
 			return fk.render_template(
 				forum + '.html',
-				ph_forum="/" + forum,
+				ph_forum = "/" + forum,
 				ph_content = content,
-				ph_error = "Please write something."
+				ph_error = "Foul language detected! Use better words.",
+				posts = get_posts(forum)
 			)
 		else:
 			write_new_post(content,forum)
 			return fk.redirect(fk.url_for(forum))
+	# GET REQUEST
 	with get_connection() as con:
 		cursor = con.cursor()
-		cursor.execute("CREATE TABLE IF NOT EXISTS " + forum + " (id INTEGER PRIMARY KEY, create_date TEXT, content TEXT, user TEXT)")
+		cursor.execute("CREATE TABLE IF NOT EXISTS " + forum + " (id INTEGER PRIMARY KEY, create_date TEXT, date_formatted TEXT, content TEXT, user TEXT)")
 		s = cursor.execute("SELECT * FROM " + forum + " ORDER BY create_date DESC")
 		print([x for x in s])
 		return(write_posts(get_posts(forum), forum))
@@ -568,10 +728,37 @@ def blogHome():
 
 @app.errorhandler(400)
 def bad_request(e):
-	if not e.description:
-		return e.description, 400
+	error_code = 400
+	error_msg = ""
+	if e.description is not None and e.description != "":
+		error_msg = e.description
 	else:
-		return 'Bad request :(', 400
+		error_msg = 'Bad request :('
+	return fk.render_template(
+		str(error_code) + ".html",
+		error_code = str(error_code) + " Bad Request Error",
+		error_msg = error_msg
+	), error_code
+
+@app.errorhandler(404)
+def not_found(e):
+	error_code = 404
+	error_msg = 'The webpage you seek is in another castle!'
+	return fk.render_template(
+		str(error_code) + ".html",
+		error_code = str(error_code) + " Not Found Error",
+		error_msg = error_msg
+	), error_code
+
+@app.errorhandler(500)
+def server_error(e):
+	error_code = 500
+	error_msg = 'That wasn\'t supposed to happen... looks like the website crashed. Maybe send the developer a note about this bug?'
+	return fk.render_template(
+		str(error_code) + ".html",
+		error_code = str(error_code) + " Internal Service Error",
+		error_msg = error_msg
+	), error_code
 
 
 
